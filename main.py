a2192850c2815b94d7fccf4b34e0b6f588cc9be4
kvngonz7 <kvn.gonz7@proton.me>
2026-04-10 19:10:16 -0700

    Prayers moved away from main, to its own file.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from prayers import show_our_father, show_hail_mary
+
 def show_menu():
     print("Catholic CLI")
     print("1. Daily Prayer")
@@ -5,24 +7,6 @@ def show_menu():
     print("3. Hail Mary")
     print("4. Exit")
 
-
-def show_our_father():
-    print("\nOur Father, who art in heaven, hallowed be thy name.")
-    print("Thy kingdom come, thy will be done, on earth as it is in heaven.")
-    print("Give us this day our daily bread,")
-    print("and forgive us our trespasses,")
-    print("as we forgive those who trespass against us,")
-    print("and lead us not into temptation,")
-    print("but deliver us from evil. Amen. \n")
-
-
-def show_hail_mary():
-    print("\nHail Mary, full of grace, the Lord is with thee.")
-    print("Blessed art thou among women,")
-    print("and blessed is the fruit of thy womb, Jesus.")
-    print("Holy Mary, Mother of God,")
-    print("pray for us sinners,")
-    print("now and at the hour of our death. Amen.\n")
 
 def main():
     while True:
---
+++ prayers.py
@@ -0,0 +1,17 @@
+def show_our_father():
+    print("\nOur Father, who art in heaven, hallowed be thy name.")
+    print("Thy kingdom come, thy will be done, on earth as it is in heaven.")
+    print("Give us this day our daily bread,")
+    print("and forgive us our trespasses,")
+    print("as we forgive those who trespass against us,")
+    print("and lead us not into temptation,")
+    print("but deliver us from evil. Amen.\n")
+
+
+def show_hail_mary():
+    print("\nHail Mary, full of grace, the Lord is with thee.")
+    print("Blessed art thou among women,")
+    print("and blessed is the fruit of thy womb, Jesus.")
+    print("Holy Mary, Mother of God,")
+    print("pray for us sinners,")
+    print("now and at the hour of our death. Amen.\n")
