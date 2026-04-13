246f490e8be0152e0e23b2541b48d0925369e1d1
kvngonz7 <kvn.gonz7@proton.me>
2026-04-12 19:17:39 -0700

    Add Examination of conscience module

--- examination.py
+++ examination.py
@@ -1,8 +1,26 @@
-def show_examination_intro():
-    print("\nExamination of Conscience")
-    print("1. Have I loved God with my whole heart?")
-    print("2. Have I spoken in charity toward others?")
-    print("3. Have I neglected prayer or Mass?\n")
-    print("4. Have I engaged in gossip or harmful speech?\n")
-    ### I will be adding Categories.
+def show_examination_menu():
+    print("\hExamanation of Conscience")
+    print("1. Duties to God")
+    print("2. Speech")
+    print("3. Charity and Duties")
+    print("4. Back")
 
+def show_duties_to_God():
+    print("\nDuties to God")
+    print("Have I neglected prayer?")
+    print("Have I missed Mass without a serious reason?")
+    print("Have I treated holy things with reverence?\n")
+
+def show_speech_examination():
+    print("\nSpeech")
+    print("Have I lied?")
+    print("Have I gossiped?")
+    print("Have I spoken harshly or without charity?\n")
+
+def show_charity_examination():
+    print("\nCharity and Duties")
+    print("Have I failed to help others when I could?")
+    print("Have I been impatient or selfish?")
+    print("Have I neglected my responsibilities?\n")
+
+    
