b03891ea8aa48cfa32a29d0bb0c67f268538b75b
kvngonz7 <kvn.gonz7@proton.me>
2026-04-12 20:47:21 -0700

    Added bible verses to project, but need to troubleshoot quotes on line 14

--- examination.py
+++ examination.py
@@ -10,18 +10,22 @@ def show_duties_to_god():
     print("Have I neglected prayer?")
     print("Have I missed Mass without a serious reason?")
     print("Have I treated holy things with reverence?\n")
+    print("\nScripture:")
+    print(""Pray without ceasing." -1 Thessalonians 5:17\n")
 
 def show_speech_examination():
     print("\nSpeech")
     print("Have I lied?")
     print("Have I gossiped?")
     print("Have I spoken harshly or without charity?\n")
+    print(""Let no evil talk come out of your mouths..." - Ephesians 4:29\n")
 
 def show_charity_examination():
     print("\nCharity and Duties")
     print("Have I failed to help others when I could?")
     print("Have I been impatient or selfish?")
     print("Have I neglected my responsibilities?\n")
+    print("\"Love one another as I have loved you." - John 13:34\n")
 
 def examination_menu():
     while True:
